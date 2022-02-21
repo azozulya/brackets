bf775704bd3ae0368ae8c37101a3e39649abd1bf
azozulya <bbbom@mail.ru>
2022-02-21 23:26:30 +0200

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = [...str];
+  const config = Object.fromEntries(bracketsConfig);
+  const arr = [];
+  let isStop = false;
+  let i = 0;
+
+  // bracketsConfig.map(([key, val]) => {
+  //   if(key === val) {
+  //     str.replace()
+  //   }
+  //   console.log(key, val);
+  // })
+  
+
+
+console.log(brackets);
+  while(!isStop && i < brackets.length) {
+    const bracket = brackets[i]; 
+    
+    if(config[bracket]) {
+      if(bracket === config[bracket]) {
+        const openBracket = arr.pop(); 
+        if(openBracket !== bracket) {
+          arr.push(openBracket);
+          arr.push(bracket);
+        }
+      } else
+        arr.push(bracket);
+    } else {
+      const openBracket = arr.length && arr.pop(); console.log('openBracket: ', openBracket, bracket);
+      if(!openBracket || config[openBracket] !== bracket) {
+        isStop = true;
+      }
+    }
+    i++;
+  };
+ console.log(!isStop);
+console.log(arr.length);
+  return isStop ? !isStop : arr.length === 0;
+
 }
